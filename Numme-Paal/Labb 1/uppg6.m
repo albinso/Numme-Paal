df451442a195109fd28f2abade6766ece995be33
MisterWeasel <heerkip@gmail.com>
2014-09-24 22:49:24 +0200

    Newton Rhapson is done

--- Numme-Paal/Labb 1/uppg6.m
+++ Numme-Paal/Labb 1/uppg6.m
@@ -1,10 +1,22 @@
-while abs(h) > abs(x)*1E-6,
-        fval = f(x);
-        J = [cos(x)  2*y 1/(x*log(10));
-             3       log(a)*2^y   -3*z;
+i = 0;
+dxnorm = 10;
+x = 0.5;
+y = 1;
+z = 2;
+format compact
+while dxnorm > abs(x)*1E-6 & i < 10,
+        f = [sin(x) + y.^2 + log(z) - 3;
+             3*x +    2.^y - z.^3;
+             x.^2 +    y.^2 + z.^3 - 6]
+        J = [cos(x)  2*y    (1/(x*log(10)));
+             3       log(2)*2^y   (-3*z);
              2*x     2*y           3*z]
-        h = (fval/fpval);
-        disp(h)
-        % disp([x, fval, fpval h])
-        x = x-h;
+        dx = -J\f;
+        x = x+dx(1);
+        y = y+dx(2);
+        z = z+dx(3);
+        dxnorm = norm(dx, Inf);
+        i = i+1
 end
+x, y, z
+i
