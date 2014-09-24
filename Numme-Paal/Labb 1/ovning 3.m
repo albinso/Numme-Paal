82a904dd14b3353a2d3e57ad1e8fcece8bcb92ae
MisterWeasel <heerkip@gmail.com>
2014-09-24 18:22:15 +0200

    Copy pasta

--- Numme-Paal/Labb 1/ovning 3.m
+++ Numme-Paal/Labb 1/ovning 3.m
@@ -4,3 +4,6 @@
 
 function kalle = f(x)
 kalle = 60 - 6 * (2 * x + 1) * 6 / (x + 1) ^ 7 * (x^2 + x + 0.1) ^ 5 - 10 * exp(-x) + 10 * x * exp(-x)
+f = @(x) 60*x - (((x.^2 + x + 0.1).^6)/((x+1).^6)) - 10*x.*exp(-x);
+hh = 1E-3;
+fp = @(x) (f(x+hh) - f(x-hh))/(2*hh);
