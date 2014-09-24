3f3a41048f95a181b593d0ae8a8a9de2997e4345
MisterWeasel <heerkip@gmail.com>
2014-09-24 22:23:50 +0200

    Jacobian is done

--- Numme-Paal/Labb 1/uppg6.m
+++ Numme-Paal/Labb 1/uppg6.m
@@ -1,24 +1,10 @@
-f = @(x) 60*x - (((x.^2 + x + 0.1).^6)/((x+1).^6)) - 10*x.*exp(-x);
-hh = 1E-3;
-fp = @(x) (f(x+hh) - f(x-hh))/(2*hh);
-x = -5:5;
-plot(x, f(x))
-x = 2;
-h = 10;
-
-format compact
-for i = 0.1:0.1:2
-    x = i;
-    h = 10;
-    while abs(h) > abs(x)*1E-6,
+while abs(h) > abs(x)*1E-6,
         fval = f(x);
-        fpval = fp(x);
+        J = [cos(x)  2*y 1/(x*log(10));
+             3       log(a)*2^y   -3*z;
+             2*x     2*y           3*z]
         h = (fval/fpval);
         disp(h)
         % disp([x, fval, fpval h])
         x = x-h;
-    end
-    x
 end
-
-% CLOSE 'NUFF!
