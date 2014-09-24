64d5628c46e6125a7c2e534fb8c766c93553a265
MisterWeasel <heerkip@gmail.com>
2014-09-24 18:10:44 +0200

    Typ uppg 5

--- Numme-Paal/Labb 1/uppg6.m
+++ Numme-Paal/Labb 1/uppg6.m
@@ -1,5 +1,22 @@
 f = @(x) 60*x - (((x.^2 + x + 0.1).^6)/((x+1).^6)) - 10*x.*exp(-x);
-x = -1.5:0.001:0;
-
+hh = 1;
+fp = @(x) (f(x+hh) - f(x-hh))/2*hh;
+x = -5:5;
 plot(x, f(x))
+x = 2;
+h = 10;
 
+format compact
+for i = [0.1:0.1:2]
+    x = i;
+    h = 10;
+    while abs(h) > abs(x)*1E-6,
+        fval = f(x);
+        fpval = fp(x);
+        h = (fval/fpval);
+        % disp([x, fval, fpval h])
+        x = x-h;
+    end
+    x
+end
+x
