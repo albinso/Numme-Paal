e5618554f79f708742d18aca8a679fb35633fdd3
MisterWeasel <heerkip@gmail.com>
2014-09-24 18:21:17 +0200

    Fungerande femma

--- Numme-Paal/Labb 1/uppg6.m
+++ Numme-Paal/Labb 1/uppg6.m
@@ -1,13 +1,13 @@
 f = @(x) 60*x - (((x.^2 + x + 0.1).^6)/((x+1).^6)) - 10*x.*exp(-x);
-hh = 1;
-fp = @(x) (f(x+hh) - f(x-hh))/2*hh;
+hh = 1E-3;
+fp = @(x) (f(x+hh) - f(x-hh))/(2*hh);
 x = -5:5;
 plot(x, f(x))
 x = 2;
 h = 10;
 
 format compact
-for i = [0.1:0.1:2]
+for i = 0.1:0.1:2
     x = i;
     h = 10;
     while abs(h) > abs(x)*1E-6,
@@ -19,4 +19,5 @@ for i = [0.1:0.1:2]
     end
     x
 end
-x
+
+% CLOSE 'NUFF!
