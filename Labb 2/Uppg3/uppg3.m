7f1b1bb040a552202e851cd9de7f32e68105104b
MisterWeasel <heerkip@gmail.com>
2014-11-04 15:34:25 +0100

    Buttman is done!

--- Labb 2/Uppg3/uppg3.m
+++ Labb 2/Uppg3/uppg3.m
@@ -3,7 +3,7 @@ plot(0, 0);
 hold on;
 t = [0:0.05:1];
 y = bezier(t, 0.81, 0.84)
-t2 = [0:0.05:0.05]
+t2 = [-0.4:0.05:0.5]
 mouth = beziermun(t2, 0.1, 0.1)
 mouth2 = beziermun(t2, 0.2, 0.1)
 %kaustika;
@@ -12,3 +12,12 @@ plot(-t, y);
 plot(t, y);
 plot(t2, mouth);
 plot(t2, mouth2);
+centerX = -0.5; centerY = 0.5; radius = 0.05;
+rectangle('Position',[centerX - radius, centerY - radius, radius*2, radius*2],...
+    'Curvature',[1,1],...
+    'FaceColor','g');
+
+centerX = 0.5; centerY = 0.5; radius = 0.05;
+rectangle('Position',[centerX - radius, centerY - radius, radius*2, radius*2],...
+    'Curvature',[1,1],...
+    'FaceColor','g');
